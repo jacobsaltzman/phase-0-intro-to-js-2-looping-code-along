85fedbacd24e45092ff735b3645c7242cd06bb7a
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-05-03 20:07:21 -0700

    commit tree 398f5b242569aaca8131069a5df99e64c60ac9b7

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(names = [],eventName){
+
+   let newMessages = [];
+    
+    for (let i = 0; i < names.length; i++){
+        newMessages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return newMessages;
+}
+
+function countDown(num){
+    while (num >= 0){
+        console.log(num);
+        num --;
+    }
+}
